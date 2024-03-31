31b3b74c68c249de348465ec7e4d651da07dcc16
Arne Ziegert <ziegert@myswooop.de>
2024-03-31 15:26:19 +0200

    basic datastructure

---
+++ database/migrations/2024_03_31_132101_create_assessments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Pupil;
+use App\Models\SchoolSubject;
+use App\Models\Semester;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assessments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Pupil::class)->constrained();
+            $table->foreignIdFor(SchoolSubject::class)->constrained();
+            $table->foreignIdFor(Semester::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assessments');
+    }
+};
